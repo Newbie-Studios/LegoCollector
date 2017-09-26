ec089286b42562cee353260e9a3576ac3d9ee443
Jamie Roberts <jamieroberts0890@gmail.com>
2017-09-26 19:21:16 +0100

    Initial Commit

---
+++ LegoCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LegoCollector
+//
+//  Created by Jamie Roberts on 26/09/2017.
+//  Copyright © 2017 Newbie-Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
